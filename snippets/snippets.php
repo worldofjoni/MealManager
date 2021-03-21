0c4d36f12bbbb18cbf7a5772c9da7c6cf175444c
world_of_joni <jonizi18902@gmail.com>
2021-03-21 13:22:15 +0100

    added detailed meal view

---
+++ snippets/snippets.php
@@ -0,0 +1,37 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-tFitle" id="exampleModalLabel">Hamburger</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#modalDelete"><i class="fas fa-times"></i></button>
+                                    <span class="mx-1"></span>
+                                    <button type="button" class="btn btn-secondary"><i class="fas fa-pen"></i></button>
+                                    <!-- <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button> -->
+                                </div>
+                            </div>
+                            <!-- Modal Delete -->
+                            <div class="modal fade" id="modalDelete" tabindex="-1" aria-labelledby="modalDeleteLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modalDeleteLabel">Delete?</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Do you really want to delete Hamburger permanently?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
--- templates/navbar.php
+++ templates/navbar.php
@@ -5,7 +5,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto mb-2 mb-sm-0">
         <li class="nav-item active">
           <a class="nav-link" href="#">Meals <span class="sr-only">(current)</span></a>
         </li>
